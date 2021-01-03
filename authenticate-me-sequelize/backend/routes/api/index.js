5823776e4f25951171ee59b38fe8a9d67d9c51fb
Ty <ltgray579@gmail.com>
2021-01-03 04:46:09 -0700

    bare bones lists page complete

--- authenticate-me-sequelize/backend/routes/api/index.js
+++ authenticate-me-sequelize/backend/routes/api/index.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const sessionRouter = require("./session.js");
 const usersRouter = require("./users.js");
+const listsRouter = require("./lists.js");
 
 // // GET /api/set-token-cookie
 // const asyncHandler = require('express-async-handler');
@@ -43,6 +44,6 @@ router.post("/test", function (req, res) {
 router.use("/session", sessionRouter);
 
 router.use("/users", usersRouter);
+router.use("/lists", listsRouter);
 
 module.exports = router;
-
---
+++ authenticate-me-sequelize/frontend/src/components/Lists/index.js
@@ -0,0 +1,30 @@
+import {fetch} from '../../store/csrf';
+import {useEffect, useState} from 'react';
+
+const ListsPage = () => {
+
+    const [currentLists, setLists] = useState([])
+
+    useEffect(async () => {
+        const response = await fetch('/api/lists');
+        // const user = await fetch('/api/users');
+        console.log(response);
+        // console.log(user);
+        setLists(response.data.lists);
+    }, []);
+
+    return (
+        <div id="list-wrapper">
+            <h1>Pick a task list</h1>
+            {!currentLists && <h3>Loading....</h3>}
+            <ul>
+                {currentLists && currentLists.map(list => {
+                    return <li>{list.listName}</li>
+                })}
+            </ul>
+            <a href="#">Create a List</a>
+        </div>
+    );
+};
+
+export default ListsPage;
