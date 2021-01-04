ee7b17b11a9cd17b5fc6af51fb26297e46ebcd39
Ty <ltgray579@gmail.com>
2021-01-03 23:57:02 -0700

    CreateList is now functional /w Redux

--- authenticate-me-sequelize/frontend/src/store/index.js
+++ authenticate-me-sequelize/frontend/src/store/index.js
@@ -2,10 +2,12 @@ import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import session from './session'
 import lists from "./lists";
+import createList from "./createlist"
 
 const rootReducer = combineReducers({
   session: session,
-  lists: lists
+  lists: lists,
+  createList: createList
 });
 
 let enhancer;
